d144222c1e603c1d448456f2c5766a6f71c194cb
Pranay Khojare <khojarepranay6@gmail.com>
2026-06-25 22:40:47 +0530

    Chore: add dotenv java for the application

--- src/main/java/org/pranay/creatorstore/CreatorStoreApplication.java
+++ src/main/java/org/pranay/creatorstore/CreatorStoreApplication.java
@@ -1,13 +1,19 @@
 package org.pranay.creatorstore;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CreatorStoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CreatorStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // configuration of dotenv-java
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        dotenv.entries().forEach((DotenvEntry entry) -> System.setProperty(entry.getKey(), entry.getValue()));
+        SpringApplication.run(CreatorStoreApplication.class, args);
+    }
 
 }
--- src/test/java/org/pranay/creatorstore/CreatorStoreApplicationTests.java
+++ src/test/java/org/pranay/creatorstore/CreatorStoreApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CreatorStoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
